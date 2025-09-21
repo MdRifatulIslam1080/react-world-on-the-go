31e7f65d97478dce4db71b150b0761e2ef36a257
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-09-21 12:01:44 +0600

    completed react-world-on-the-go

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({ country }) => {
-  console.log(country.flags.flags.png);
-  console.log(country.name.common);
-  console.log(country.population.population);
+const Country = ({ country, handleVisitedCountries }) => {
+  const [visited, setVisited] = useState(false);
+  // console.log(country.flags.flags.png);
+  // console.log(country.name.common);
+  // console.log(country.population.population);
+  // console.log(country.area.area);
+
+  const handleVisited = () => {
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+
+    // setVisited(visited ? false : true);
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
+
   return (
-    <div>
-      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+    <div className={`country ${visited && "country-visited"}`}>
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
 
       <h3>Name: {country.name.common} </h3>
       <p>Population:{country.population.population}</p>
+      <p>
+        Area: {country.area.area},
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}
+      </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
